3d0748b582a027df8996081edbe8c826b3993d85
DarkifinityCode <168334408+DarkifinityCode@users.noreply.github.com>
2025-08-09 23:56:24 +0530

    Delete Scripts.js

--- Scripts.js
+++
@@ -1,9 +0,0 @@
-// Mobile Menu Toggle
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector("nav ul");
-
-    menuToggle?.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-    });
-});
